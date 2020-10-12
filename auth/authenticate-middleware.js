e419e672f7f4f13a5772d1b22b68c2389aab24a0
unknown <keng.thao@hotmail.com>
2020-10-11 22:53:11 -0500

    added middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,23 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
+  const { authorization } = req.headers
+
+  const secret = process.env.JWT_SECRET || "this is my secret";
+
+  if (authorization) {
+    jwt.verify(authorization, secret, (err, decodedToken) => {
+      if(err){
+        res.status(401).json({ message: "Invalid Credentials" })
+      } else {
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
+  } else {
   res.status(401).json({ you: 'shall not pass!' });
+  }
 };
