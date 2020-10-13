3e09a96f4db27fc717ad86ccfe0d90ef27b59bf2
unknown <keng.thao@hotmail.com>
2020-10-12 20:53:12 -0500

    added questions

---
+++ questions.js
@@ -0,0 +1,17 @@
+// 1. Differences between using _sessions_ or _JSON Web Tokens_ for authentication.
+
+        //Session cookies are stored on the server memory and token are stored on the client side.
+
+// 2. What does `bcrypt` do to help us store passwords in a secure manner.
+
+    //Bcrypt has password hashing function, implements salting manually and automatically, and accumulative hashing rounds.
+
+// 3. How are unit tests different from integration and end-to-end testing.
+
+    //Unit testing isolates smaller units of software (function or methods).
+    //Integration testing checks to see if different modules are working fine when combined together as a group.
+    //End-to-end testing generally tests entire workflow function (whole application).
+
+// 4. How _Test Driven Development_ changes the way we write applications and tests.
+
+    //It helps you write higher quality code.
